a7cdb8d432eaf6bf400cf19740e9f9a9ee0c2203
cemrehasirci <cecemrehasirci@hotmail.com>
2025-07-26 19:36:32 +0300

    auth control

---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Yetkisiz erişim. Token eksik!" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json( error.message );
+  }
+};
+
+module.exports = auth;
--- src/routers/course_router.js
+++ src/routers/course_router.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const { getAllCourse, getCourseById , createCourse, updateCourse, deleteCourse, searchCourse } = require('../controllers/course_controller.js');
-
+const auth = require('../middleware/auth.js');
+const router = express.Router();
 
 router.get('/getAllCourse', getAllCourse);
 router.get('/getCourse/:id', getCourseById );
 router.get('/searchCourse', searchCourse);
-router.post('/createCourse', createCourse);
+router.post('/createCourse', auth, createCourse);
 router.patch('/updateCourse/:id', updateCourse);
-router.delete('/deleteCourse/:id', deleteCourse)
+router.delete('/deleteCourse/:id', auth, deleteCourse)
 
 module.exports = router; 
